bf8c462d30a0d78e7ac0045763976ff0b21fe2af
Kha Tran <khadieptran@gmail.com>
2021-08-23 11:46:08 -0400

    Create object to store values and function to display on screen

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Functions for different operations
 function add(a, b) {
     return a + b;
 }
@@ -16,4 +17,18 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
     return operator(a, b);
+}
+
+// Create an object that holds all the values and operator
+let storedCalculation = {
+    firstNum: 0,
+    sencondNum: null,
+    operator: add, 
+    result: 0,
+}
+
+// Function to display values on the screen
+const screen = document.querySelector('#display');
+function display(num) {
+    screen.textContent = num;
 }
